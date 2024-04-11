7810c5071c794d9aeb5ef29f1cacb9f08a9f2b8d
Rlok <law108000@gmail.com>
2024-04-11 09:28:34 +0800

    feature: add consult data, slice

--- src/views/financial-consultant/FinancialConsultant.tsx
+++ src/views/financial-consultant/FinancialConsultant.tsx
@@ -1,19 +1,41 @@
-import { Box, Container, Typography } from '@mui/material';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import React, { useState } from 'react';
+import { Divider, Box } from '@mui/material';
+import Breadcrumb from 'src/layouts/full/shared/breadcrumb/Breadcrumb';
+import PageContainer from 'src/components/container/PageContainer';
+import ChatSidebar from 'src/components/financial-consultant/ChatSidebar';
+import ChatContent from 'src/components/financial-consultant/ChatContent';
+import ChatMsgSent from 'src/components/financial-consultant/ChatMsgSent';
+import AppCard from 'src/components/shared/AppCard';
 
-const FinanicalConsultant = () => (
-  <Box
-    display="flex"
-    flexDirection="column"
-    height="100vh"
-    textAlign="center"
-    justifyContent="center"
-  >
-    <Container maxWidth="md">
-      <Typography align="center" variant="h4" mb={4}>
-        This is the Financial Consultant page.
-      </Typography>
-    </Container>
-  </Box>
-);
+const FinanicalConsultant = () => {
+  const [isMobileSidebarOpen, setMobileSidebarOpen] = useState(false);
+
+  return (
+    <PageContainer title="Personal Financial Consultant" description="this is Personal Financial Consultant page">
+      <Breadcrumb title="Personal Financial Consultant" subtitle="Your AI powered financial advisor" />
+      <AppCard>
+        {/* ------------------------------------------- */}
+        {/* Left part */}
+        {/* ------------------------------------------- */}
+
+        <ChatSidebar
+          isMobileSidebarOpen={isMobileSidebarOpen}
+          onSidebarClose={() => setMobileSidebarOpen(false)}
+        />
+        {/* ------------------------------------------- */}
+        {/* Right part */}
+        {/* ------------------------------------------- */}
+
+        <Box flexGrow={1}>
+          <ChatContent toggleChatSidebar={() => setMobileSidebarOpen(true)} />
+          <Divider />
+          <ChatMsgSent />
+        </Box>
+      </AppCard>
+    </PageContainer>
+  );
+};
 
 export default FinanicalConsultant;
